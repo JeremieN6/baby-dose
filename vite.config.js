3133eea8cc547241b6ad9e21e0ed1b3c17081571
Jeremie <ngoyi.jeremie@gmail.com>
2025-09-02 02:32:25 +0200

    correction : variable dynamique pour l'url de vite.config.js pour avoir 2 urls disponible (mutualisé hostinger + netlify)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
