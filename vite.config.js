ec6ae2975c76dda572361b6682195d211dd1662b
Jeremie <ngoyi.jeremie@gmail.com>
2025-09-02 02:31:22 +0200

    variable dynamique pour l'url de vite.config.js pour avoir 2 urls disponible (mutualisé hostinger + netlify)

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
+  // Priorité : VITE_BASE_PATH > VITE_BASE_URL > fallback
+  // VITE_BASE_PATH est l'URL faisant référence à là ou se trouve le site sur l'hébergeur mutualisé hostinger (donc /projets/labo/babydose/ (chez jeremiecode.fr))
+  // VITE_BASE_URL pour faire fonctionner le site sur netlify en même que le mutualisé. Comme il n'est pas à la racine ils n'ont pas la meme url donc le basePath doit être dynamique 
+  const basePath = env.VITE_BASE_PATH || env.VITE_BASE_URL || '/projets/labo/babydose/'
+
   return {
     plugins: [vue()],
-    // Base dépend de la variable d'environnement VITE_BASE_PATH
-    base: env.VITE_BASE_PATH || './',
+    base: basePath,
   }
 })
